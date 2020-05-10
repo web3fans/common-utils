d14e781cfeb3f220a6766921966a992bfde94a3d
CoolSummer <147201214@qq.com>
2020-05-10 17:35:33 +0800

    add cloneDeep

--- src/utils/deepClone.js
+++ src/utils/deepClone.js
@@ -1,11 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-03-15 23:38:34
- * @LastEditTime : 2020-04-01 21:06:07
- * @LastEditors  : Please set LastEditors
+ * @LastEditTime: 2020-05-10 17:34:19
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /work/utils/common-utils/src/utils/deepClone.js
  */
+
+
+
 ///JSON.parse(JSON.stringify(obj))缺点：
 // 1.忽略undefined
 // 2.不能序列化函数
@@ -34,3 +37,23 @@ export const cloneDeep = (obj) => {
 ///可以处理undefined和循环引用对象
 
 // const cloneObject = await cloneDeep(obj)
+
+
+
+export const cloneDeep = (obj) => {
+    if(obj === null) return obj;
+    if(typeof obj !== 'object') return obj;
+    if(obj instanceof RegExp) {
+        return new RegExp(obj)
+    }
+    if(obj instanceof Date){
+        return new Date(obj)
+    }
+    let  newObj = new obj.constructor;
+    for(let key in obj){
+        if(obj.hasOwnProperity(key)){
+            newObj[key] = cloneDeep(obj[key])
+        }
+    }
+    return newObj;
+}
