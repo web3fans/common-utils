b87dbeda5f575fa7247e6d05e3b71fad83927476
CoolSummer <147201214@qq.com>
2020-05-16 00:31:16 +0800

    merge sort

--- src/sort/insertSort.js
+++ src/sort/insertSort.js
@@ -1,30 +1,30 @@
 /*
  * @Author: your name
  * @Date: 2020-05-15 00:14:51
- * @LastEditTime: 2020-05-15 00:27:57
+ * @LastEditTime: 2020-05-15 00:32:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /work/common-utils/src/sort/insertSort.js
  */
 const insertSort = (arr) => {
   let length = arr.length;
-  if(length <= 1) {
-      return arr;
+  if (length <= 1) {
+    return arr;
   }
-  for(let i = 1; i < length; i++){
-      let value = arr[i];
-      let j = i-1;
-      for(;j >=0;j--){
-          if(value < arr[j]){
-              arr[j+ 1] = arr[j];
-          }else{ 
-              break;
-          }
+  for (let i = 1; i < length; i++) {
+    let value = arr[i];
+    let j = i - 1;
+    for (; j >= 0; j--) {
+      if (value < arr[j]) {
+        arr[j + 1] = arr[j];
+      } else {
+        break;
       }
-      a[j+1] = value; // 插入数据
+    }
+    a[j + 1] = value; // 插入数据
   }
   return arr;
-}
+};
 
 //test code
 // let a = [2, 0, 4, 1, 3, 5, 8, 12, 6];
---
+++ src/sort/mergesort.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-15 00:31:44
+ * @LastEditTime: 2020-05-15 00:31:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /work/common-utils/src/sort/mergesort.js
+ */
